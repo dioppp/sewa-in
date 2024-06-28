4702dd7e824836b7fb2fe2773dda188d0605aeab
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-06-28 15:57:14 +0700

    [FEAT] update venue seeder

--- database/seeders/VenueSeeder.php
+++ database/seeders/VenueSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use Database\Factories\VenueFactory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class VenueSeeder extends Seeder
 {
@@ -13,6 +13,40 @@ class VenueSeeder extends Seeder
      */
     public function run(): void
     {
-        VenueFactory::new()->count(3)->create();
+        DB::table('venues')->insert([
+            'user_id' => 1,
+            'name' => 'GOR Aci Tri Dharm',
+            'address' => 'Jl. Sunan Ampel No.214, Jrebeng Lor, Kec. Kedopok',
+            'description' => 'Lapangan badminton indoor',
+            'photo' => 'venues/' . fake()->image('public/storage/venues', 640, 480, null, false),
+            'created_by' => 'Seeder',
+            'updated_by' => 'Seeder',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('venues')->insert([
+            'user_id' => 1,
+            'name' => 'GOR A Yani',
+            'address' => 'l. Dr.Sutomo No.60, Tisnonegaran, Kec. Kanigaran',
+            'description' => 'Kompleks olahraga',
+            'photo' => 'venues/' . fake()->image('public/storage/venues', 640, 480, null, false),
+            'created_by' => 'Seeder',
+            'updated_by' => 'Seeder',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('venues')->insert([
+            'user_id' => 1,
+            'name' => 'Lucky Stone Futsal',
+            'address' => 'Jl. Basuki Rahmad No.1, Mangunharjo, Kec. Mayangan',
+            'description' => 'Lapangan futsal indoor',
+            'photo' => 'venues/' . fake()->image('public/storage/venues', 640, 480, null, false),
+            'created_by' => 'Seeder',
+            'updated_by' => 'Seeder',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
